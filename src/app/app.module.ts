1e7bbd23fd1fa183c66c910c8bf174fdaf5bbae9
Pawel Zamorski <zamorskipawel@wp.pl>
2019-11-15 22:28:37 +0100

    First commit: basic Angular app with Bootstrap grid, Angular Material, Routing module, Sass

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// Angular Material
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    // Angular Material
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
